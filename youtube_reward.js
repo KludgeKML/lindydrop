e514908218473b288afdb844305e52fef1565c6e
Keith M Lawrence <keith@kludge.co.uk>
2015-03-15 23:06:19 +0000

    add YT data call

--- youtube_reward.js
+++ youtube_reward.js
@@ -1,3 +1,5 @@
+var dataKey="NOTHING";
+
 var YouTubeReward = function (selector_id) {
 	this.selector = '#' + selector_id;
 	this.selector_id = selector_id;
@@ -6,7 +8,7 @@ var YouTubeReward = function (selector_id) {
 	this.player = new YT.Player(this.selector_id, {
     	height: '300',
     	width: '300',
-    	videoId: 'M7lc1UVf-VE',
+/*    	videoId: 'M7lc1UVf-VE',*/
     	playerVars: {'controls': 0},
     	events: {
       	'onReady': this.onPlayerReady,
@@ -22,8 +24,39 @@ YouTubeReward.prototype.triggerReward = function() {
 
 /* Called when the player is up */
 YouTubeReward.prototype.onPlayerReady = function(event) {
+	getIt();
+	// this.prepareNextVideo();
 };
 
 /* Called when the player changes state */
 YouTubeReward.prototype.onPlayerStateChange = function(event) {
 };
+
+YouTubeReward.prototype.prepareNextVideo = function() {
+	/* find videoId */
+}
+
+function getIt() {
+	queryString = "https://www.googleapis.com/youtube/v3/search" +
+				"?key=" + dataKey +
+				"&part=snippet" +
+				"&type=video" +
+			  	"&videoEmbeddable=true" +
+				"&q=Lindy+Hop"
+
+
+	var jqxhr = $.ajax(queryString)
+  		.done(function() {
+    		alert( "success" );
+  		})
+  		.fail(function() {
+    		alert( "error" );
+  		})
+  		.always(function() {
+    		alert( "complete" );
+  		});
+
+	// this.player.cueVideoById(videoId:String);
+}
+
+
